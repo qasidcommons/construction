891776ed8802dd32b1581ae660f301c2bdd110c9
qasidcommons <help@qasid.com>
2023-08-12 09:51:14 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,9 +7,13 @@ export default function Home() {
       <div className={styles.description}>
         <h1>Qasid student information system currently under maintenance
         </h1>
-        <h2>Please check back in a few hours</h2>
-        <h3><a href="https://www.qasid.com">Go to www.qasid.com in the meantime</a></h3>
       </div>
+      <div className={styles.description}>
+        <h2>Please check back in a few hours</h2>
+    </div>
+          <div className={styles.description}>
+        <h3><a href="https://www.qasid.com">Go to www.qasid.com in the meantime</a></h3>
+    </div>
     </main>
   )
 }
